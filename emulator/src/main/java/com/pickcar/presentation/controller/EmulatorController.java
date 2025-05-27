d722bd2e010a78851bb4ca1eda51d8040d8cb272
bailey <baileypark@kakao.com>
2025-05-27 14:14:26 +0900

    feat: emulator 빌드

--- emulator/src/main/java/com/pickcar/presentation/controller/EmulatorController.java
+++ emulator/src/main/java/com/pickcar/presentation/controller/EmulatorController.java
@@ -1,11 +1,11 @@
-package com.pickcar.presentation;
+package com.pickcar.presentation.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/emulator")
+@RequestMapping("/emulator")
 public class EmulatorController {
 
     @GetMapping
