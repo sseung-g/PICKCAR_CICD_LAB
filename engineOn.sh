f35e7c41cd08d424c518613e190f7d0a668c8608
bailey <baileypark@kakao.com>
2025-06-02 11:06:55 +0900

    refactor: 1회 전송을 100회로 변경

--- engineOn.sh
+++ engineOn.sh
@@ -5,16 +5,24 @@ export PATH=/usr/local/bin:/usr/bin:/bin:/opt/homebrew/bin
 
 URL="http://localhost:8080/api/v1/engine/on"
 TYPE="Content-Type: application/json"
-BODY='{
-        "carId":"1",
+
+for i in {1..100}; do
+	BODY=$(cat <<EOF
+{
+        "carId":"$i",
         "mdn":"01234567890",
         "terminalId":"A001",
         "manufactureId":"6",
         "packetVersion":"5",
         "deviceId":"1"
-}'
+}
+EOF
+)
 
-curl -X POST "$URL" \
+curl -s -o /dev/null -w "%{http_code}\n" -X POST "$URL" \
      -H "$TYPE" \
-     -d "$BODY"
+     -d "$BODY" &
+done
+
+wait  # 모든 백그라운드 curl 요청이 끝날 때까지 대기
 
