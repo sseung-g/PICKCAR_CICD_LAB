8c7ab0f738a5ac8fd9c04d16460863d6f09f7a66
bailey <baileypark@kakao.com>
2025-06-02 11:06:18 +0900

    refactor: 버전 맞추고 view 삭제 처리

--- emulator/src/main/java/com/pickcar/presentation/controller/EmulatorController.java
+++ emulator/src/main/java/com/pickcar/presentation/controller/EmulatorController.java
@@ -1,15 +1,9 @@
 package com.pickcar.presentation.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/emulator")
 public class EmulatorController {
-
-    @GetMapping
-    public String emulator() {
-        return "index";
-    }
 }
