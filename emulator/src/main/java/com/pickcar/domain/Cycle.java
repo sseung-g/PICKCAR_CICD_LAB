66389fee5f431934be6b709cbe2ca8f2b71b5d51
seomoon <sunghoon1216@naver.com>
2025-06-04 18:42:26 +0900

    refactor: cycleinfo 테이블을 cycles 테이블로 변경

--- emulator/src/main/java/com/pickcar/domain/Cycle.java
+++ emulator/src/main/java/com/pickcar/domain/Cycle.java
@@ -7,21 +7,19 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Map;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters.LocalDateTimeConverter;
 
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class CycleInfo {
+public class Cycle {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- emulator/src/main/java/com/pickcar/infrastructure/CycleRepository.java
+++ emulator/src/main/java/com/pickcar/infrastructure/CycleRepository.java
@@ -1,12 +1,12 @@
 package com.pickcar.infrastructure;
 
-import com.pickcar.domain.CycleInfo;
+import com.pickcar.domain.Cycle;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CycleInfoRepository extends JpaRepository<CycleInfo, Long> {
-    List<CycleInfo> findByVehicleIdAndOccurredAtBetween(Long vehicleId, LocalDateTime occurredAtStart, LocalDateTime occurredAtEnd);
+public interface CycleRepository extends JpaRepository<Cycle, Long> {
+    List<Cycle> findByVehicleIdAndOccurredAtBetween(Long vehicleId, LocalDateTime occurredAtStart, LocalDateTime occurredAtEnd);
 
 //    @Query(value = """
 //        SELECT
