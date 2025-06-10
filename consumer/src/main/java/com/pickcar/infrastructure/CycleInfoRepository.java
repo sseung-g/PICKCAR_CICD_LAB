6613c63cd941da8d499a3d3b6560c390fc59d0ea
bailey <baileypark@kakao.com>
2025-06-10 23:27:00 +0900

    refactor: @Repository 추가

--- consumer/src/main/java/com/pickcar/infrastructure/CycleInfoRepository.java
+++ consumer/src/main/java/com/pickcar/infrastructure/CycleInfoRepository.java
@@ -2,6 +2,8 @@ package com.pickcar.infrastructure;
 
 import com.pickcar.domain.Cycle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CycleInfoRepository extends JpaRepository<Cycle, Long> {
 }
--- consumer/src/main/java/com/pickcar/infrastructure/EventInfoRepository.java
+++ consumer/src/main/java/com/pickcar/infrastructure/EventInfoRepository.java
@@ -2,6 +2,8 @@ package com.pickcar.infrastructure;
 
 import com.pickcar.domain.EventInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventInfoRepository extends JpaRepository<EventInfo, Long> {
 }
