fb6d599417e24f8da5097e141abde78a5b6ced89
bailey <baileypark@kakao.com>
2025-06-10 23:30:58 +0900

    feat: Listener 생성

---
+++ consumer/src/main/java/com/pickcar/application/CycleMessageListener.java
@@ -0,0 +1,21 @@
+package com.pickcar.application;
+
+import com.pickcar.mq.config.CycleRabbitMQConfig;
+import com.pickcar.mq.dto.CyclePayload;
+import com.pickcar.mq.dto.EventPayload;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CycleMessageListener {
+
+
+    private final CycleInfoService cycleInfoService;
+
+    @RabbitListener(queues = CycleRabbitMQConfig.CYCLE_QUEUE)
+    public void cycleMessage(CyclePayload cyclePayload) {
+        cycleInfoService.cycle(cyclePayload);
+    }
+}
