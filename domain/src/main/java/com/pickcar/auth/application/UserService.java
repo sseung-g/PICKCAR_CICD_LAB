9c4f5f8500f2e8e59a77c56ce5eae522d98cbddb
SeungGyeong <tpsseung@gmail.com>
2025-06-11 20:15:44 +0900

    feat: user 등록 기능 구현

--- domain/src/main/java/com/pickcar/auth/application/UserService.java
+++ domain/src/main/java/com/pickcar/auth/application/UserService.java
@@ -5,8 +5,11 @@ import com.pickcar.auth.domain.UserInfo;
 import com.pickcar.auth.domain.UserRole;
 import com.pickcar.auth.domain.UserStatus;
 import com.pickcar.auth.infrastructure.UserRepository;
-import com.pickcar.company.application.CompanyService;
+import com.pickcar.auth.presentation.dto.request.UserInfoRequest;
+import com.pickcar.jwt.UserPrincipal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,29 +18,65 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class UserService {
 
-    private final CompanyService companyService;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    @Transactional
-    public void create(Long companyId, UserRole role) {
-
-        //FIXME: 리팩토링으로 책임 분리
-        if (role.equals(UserRole.SUPER_ADMIN)) {
-            throw new IllegalArgumentException("[ERROR] User Can't be a Super Admin");
-        }
-
-        User user = User.builder()
-                .companyId(1L)      //TODO: 있는지 검사 필요
-                .info(new UserInfo("email", "password", "name", "phone")) // FIXME
-                .status(UserStatus.ACTIVE)
-                .role(role)
-                .build();
-
-        userRepository.save(user);
-    }
+//    @Transactional
+//    public void create(Long companyId, UserRole role) {
+//
+//        //FIXME: 리팩토링으로 책임 분리
+//        if (role.equals(UserRole.SUPER_ADMIN)) {
+//            throw new IllegalArgumentException("[ERROR] User Can't be a Super Admin");
+//        }
+//
+//        User user = User.builder()
+//                .companyId(1L)      //TODO: 있는지 검사 필요
+//                .info(new UserInfo("email", "password", "name", "phone")) // FIXME
+//                .status(UserStatus.ACTIVE)
+//                .role(role)
+//                .build();
+//
+//        userRepository.save(user);
+//    }
 
     public User getById(Long id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] User Not Found By Id " + id));
+    }
+
+    @Transactional
+    public void createEmployee(@AuthenticationPrincipal UserPrincipal principal,
+                               UserInfoRequest request) {
+        //토큰에 있는 회사ID 사용
+        User employee = User.builder()
+                .companyId(principal.getCompanyId())
+                .info(toEncodedUserInfo(request))
+                .role(UserRole.EMPLOYEE)
+                .status(UserStatus.ACTIVE)
+                .build();
+
+        userRepository.save(employee);
+    }
+
+    @Transactional
+    public void createAdmin(UserInfoRequest request) {
+        //요청에 있는 회사ID 사용
+        User admin = User.builder()
+                .companyId(request.companyId())
+                .info(toEncodedUserInfo(request))
+                .role(UserRole.ADMIN)
+                .status(UserStatus.ACTIVE)
+                .build();
+
+        userRepository.save(admin);
+    }
+
+    private UserInfo toEncodedUserInfo(UserInfoRequest request) {
+        return new UserInfo(
+                request.email(),
+                passwordEncoder.encode(request.password()),
+                request.name(),
+                request.phoneNumber()
+        );
     }
 }
---
+++ domain/src/main/java/com/pickcar/auth/presentation/UserController.java
@@ -0,0 +1,32 @@
+package com.pickcar.auth.presentation;
+
+import com.pickcar.auth.application.UserService;
+import com.pickcar.auth.presentation.dto.request.UserInfoRequest;
+import com.pickcar.jwt.UserPrincipal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/users")
+    public void registerEmployee(@AuthenticationPrincipal UserPrincipal principal,
+                               @RequestBody UserInfoRequest request) {
+        userService.createEmployee(principal,request);
+    }
+
+    @PreAuthorize("hasRole('SUPER_ADMIN')")
+    @PostMapping("/admins")
+    public void registerAdmin(@RequestBody UserInfoRequest request) {
+        userService.createAdmin(request);
+    }
+}
