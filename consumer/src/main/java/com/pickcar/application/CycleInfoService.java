1c7a8c942df54d4b0df55592a37bbf2b17c0de2d
seomoon <sunghoon1216@naver.com>
2025-06-11 19:38:44 +0900

    fix: 주행거리 계산식 변경

--- consumer/src/main/java/com/pickcar/application/CycleInfoService.java
+++ consumer/src/main/java/com/pickcar/application/CycleInfoService.java
@@ -2,6 +2,7 @@ package com.pickcar.application;
 
 import com.pickcar.emulator.domain.Cycle;
 import com.pickcar.infrastructure.CycleInfoRepository;
+import com.pickcar.mq.dto.CycleInfoPayload;
 import com.pickcar.mq.dto.CyclePayload;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,13 +16,8 @@ public class CycleInfoService {
     private final CycleInfoRepository cycleInfoRepository;
 
     public void cycle(CyclePayload request) {
-        Cycle cycleInfo = Cycle.builder()
-                .vehicleId(request.getVehicleId())
-                .occurredAt(request.getOccurredAt())
-                .cycleCnt(request.getCycleCnt())
-                .distance(request.getDistance())
-                .cycleInfos(request.getCycleInfos())
-                .build();
+        Cycle cycleInfo = new Cycle(request.getVehicleId(), request.getOccurredAt(), request.getCycleCnt(), request.getCycleInfos());
+
         cycleInfoRepository.save(cycleInfo);
     }
 }
