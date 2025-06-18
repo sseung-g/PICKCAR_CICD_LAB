eb1ece486a001beafda0d7044fe1d54615196f0e
bailey <baileypark@kakao.com>
2025-06-18 17:21:38 +0900

    refactor: 데이터 yaml에서 관리 및 common에서 consumer로 이동

--- consumer/src/main/java/com/pickcar/config/EventRabbitMQConfig.java
+++ consumer/src/main/java/com/pickcar/config/EventRabbitMQConfig.java
@@ -1,33 +1,39 @@
-package com.pickcar.mq.config;
+package com.pickcar.config;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EventRabbitMQConfig {
 
-    public static final String EVENT_QUEUE = "event-queue";
-    public static final String EVENT_EXCHANGE = "exchange-event";
-    public static final String EVENT_ROUTING_KEY = "event.key";
+    @Value("${mq.event.queue}")
+    private String queueName;
+
+    @Value("${mq.event.exchange}")
+    private String exchange;
+
+    @Value("${mq.event.routing-key}")
+    private String routingKey;
 
     @Bean
     public Queue eventQueue() {
-        return new Queue(EVENT_QUEUE, true);
+        return new Queue(queueName, true);
     }
 
     @Bean
     public DirectExchange eventExchange() {
-        return new DirectExchange(EVENT_EXCHANGE);
+        return new DirectExchange(exchange);
     }
 
     @Bean
     public Binding eventBinding(Queue eventQueue, DirectExchange eventExchange) {
         return BindingBuilder.bind(eventQueue)
                 .to(eventExchange)
-                .with(EVENT_ROUTING_KEY);
+                .with(routingKey);
     }
 }
