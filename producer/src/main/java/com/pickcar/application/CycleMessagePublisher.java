51c31a42e2a6c665d33d7c920ac8546c8fb585ec
bailey <baileypark@kakao.com>
2025-06-18 17:23:30 +0900

    refactor: 데이터 yaml에서 관리

--- producer/src/main/java/com/pickcar/application/CycleMessagePublisher.java
+++ producer/src/main/java/com/pickcar/application/CycleMessagePublisher.java
@@ -1,8 +1,9 @@
 package com.pickcar.application;
 
-import com.pickcar.mq.dto.CyclePayload;
+import com.pickcar.dto.CyclePayload;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,13 @@ public class CycleMessagePublisher {
 
     private final RabbitTemplate rabbitTemplate;
 
+    @Value("${mq.cycle.exchange}")
+    private String exchange;
+
+    @Value("${mq.cycle.routing-key}")
+    private String routingKey;
+
     public void publish(CyclePayload cyclePayload) {
-        rabbitTemplate.convertAndSend("exchange-cycle", "cycle.key", cyclePayload);
+        rabbitTemplate.convertAndSend(exchange, routingKey, cyclePayload);
     }
 }
--- producer/src/main/java/com/pickcar/application/EventMessagePublisher.java
+++ producer/src/main/java/com/pickcar/application/EventMessagePublisher.java
@@ -1,8 +1,9 @@
 package com.pickcar.application;
 
-import com.pickcar.mq.dto.EventPayload;
+import com.pickcar.dto.EventPayload;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,13 @@ public class EventMessagePublisher {
 
     private final RabbitTemplate rabbitTemplate;
 
+    @Value("${mq.event.exchange}")
+    private String exchange;
+
+    @Value("${mq.event.routing-key}")
+    private String routingKey;
+
     public void publish(EventPayload eventPayload) {
-        rabbitTemplate.convertAndSend("exchange-event", "event.key", eventPayload);
+        rabbitTemplate.convertAndSend(exchange, routingKey, eventPayload);
     }
 }
