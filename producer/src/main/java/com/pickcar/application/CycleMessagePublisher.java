2990111f400f0cd7118a916326c9d44f9d5b370e
bailey <baileypark@kakao.com>
2025-06-20 13:40:20 +0900

    feat: MQ 예외처리 및 로깅

--- producer/src/main/java/com/pickcar/application/CycleMessagePublisher.java
+++ producer/src/main/java/com/pickcar/application/CycleMessagePublisher.java
@@ -2,10 +2,12 @@ package com.pickcar.application;
 
 import com.pickcar.dto.CyclePayload;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CycleMessagePublisher {
@@ -18,7 +20,14 @@ public class CycleMessagePublisher {
     @Value("${mq.cycle.routing-key}")
     private String routingKey;
 
-    public void publish(CyclePayload cyclePayload) {
-        rabbitTemplate.convertAndSend(exchange, routingKey, cyclePayload);
+    // TODO: publish를 합칠까 고려중
+    public void publish(CyclePayload Payload) {
+        try {
+            rabbitTemplate.convertAndSend(exchange, routingKey, Payload);
+            log.info("MQ 전송 성공: exchange={}, routingKey={}, payload={}", exchange, routingKey, Payload);
+        } catch (Exception e) {
+            log.error("MQ 전송 실패: exchange={}, routingKey={}, payload={}", exchange, routingKey, Payload);
+            // TODO: 실패 시 재처리 고려
+        }
     }
 }
--- producer/src/main/java/com/pickcar/application/EventMessagePublisher.java
+++ producer/src/main/java/com/pickcar/application/EventMessagePublisher.java
@@ -2,10 +2,12 @@ package com.pickcar.application;
 
 import com.pickcar.dto.EventPayload;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EventMessagePublisher {
@@ -18,7 +20,14 @@ public class EventMessagePublisher {
     @Value("${mq.event.routing-key}")
     private String routingKey;
 
-    public void publish(EventPayload eventPayload) {
-        rabbitTemplate.convertAndSend(exchange, routingKey, eventPayload);
+    // TODO: publish를 합칠까 고려중
+    public void publish(EventPayload payload) {
+        try {
+            rabbitTemplate.convertAndSend(exchange, routingKey, payload);
+            log.info("MQ 전송 성공: exchange={}, routingKey={}, payload={}", exchange, routingKey, payload);
+        } catch (Exception e) {
+            log.error("MQ 전송 실패: exchange={}, routingKey={}, payload={}", exchange, routingKey, payload);
+            // TODO: 실패 시 재처리 고려
+        }
     }
 }
