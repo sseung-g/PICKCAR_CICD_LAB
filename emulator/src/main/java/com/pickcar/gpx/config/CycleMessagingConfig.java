f25bdbf7d15ec73cdb811d8b5c80aad4d6874f3d
bailey <baileypark@kakao.com>
2025-06-30 03:13:10 +0900

    feat: TopicExchange로 RabbitMQ 메시지 Listen Topic(1:N) * 참고로 반드시 queue는 이름이 겹치면 안됨

---
+++ emulator/src/main/java/com/pickcar/gpx/config/CycleMessagingConfig.java
@@ -0,0 +1,39 @@
+package com.pickcar.gpx.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CycleMessagingConfig {
+
+    @Value("${mq.cycle.queue}")
+    private String queueName;
+
+    @Value("${mq.cycle.exchange}")
+    private String exchange;
+
+    @Value("${mq.cycle.routing-key}")
+    private String routingKey;
+
+    @Bean
+    public Queue cycleQueue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public TopicExchange cycleExchange() {
+        return new TopicExchange(exchange);
+    }
+
+    @Bean
+    public Binding cycleBinding(Queue cycleQueue, TopicExchange cycleExchange) {
+        return BindingBuilder.bind(cycleQueue)
+                .to(cycleExchange)
+                .with(routingKey+".*");
+    }
+}
