9246fd81643fc8eee6f3d5aa1b77d74e6fac3561
seomoon <sunghoon1216@naver.com>
2025-07-06 21:28:44 +0900

    style: 테스트용 jwt 필터 주석 해제

--- common/src/main/java/com/pickcar/security/filter/JwtAuthenticationFilter.java
+++ common/src/main/java/com/pickcar/security/filter/JwtAuthenticationFilter.java
@@ -40,70 +40,67 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+        if (EXCLUDE_URLS.stream()
+                .map(patternParser::parse)
+                .anyMatch(pathPattern -> pathPattern.matches(PathContainer.parsePath(request.getRequestURI())))) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
-        filterChain.doFilter(request, response);
+        // AccessToken 추출
+        String accessToken = extractAccessToken(request);
 
-//        if (EXCLUDE_URLS.stream()
-//                .map(patternParser::parse)
-//                .anyMatch(pathPattern -> pathPattern.matches(PathContainer.parsePath(request.getRequestURI())))) {
-//            filterChain.doFilter(request, response);
-//            return;
-//        }
-//
-//        // AccessToken 추출
-//        String accessToken = extractAccessToken(request);
-//
-//        if(accessToken != null){
-//            TokenStatus status = jwtProvider.validateToken(accessToken);
-//            log.info("TokenStatus : {}", status);
-//
-//            switch (status) {
-//                case VALID -> {
-//                    Claims claims = jwtProvider.parseToken(accessToken).getBody();
-//                    Long id = Long.valueOf(claims.getSubject());
-//                    String name = jwtProvider.validateAndGetClaim(claims,"name",String.class); //TODO: 값이 없을 경우의 예외처리 하기
-//                    String role = jwtProvider.validateAndGetClaim(claims,"role",String.class);
-//
-//                    JwtUserDetails userDetails = new JwtUserDetails(id, name, role);
-//
-//                    Authentication authentication = new UsernamePasswordAuthenticationToken(
-//                            userDetails, null, List.of(new SimpleGrantedAuthority("ROLE_" + role)));
-//
-//                    SecurityContextHolder.getContext().setAuthentication(authentication);
-//                    filterChain.doFilter(request, response);
-//                    return;
-//                }
-//                case EXPIRED -> {
-//                    // 클라이언트에 토큰 만료 안내 -> 클라이언트 측에서 AccessToken 재발급 요청 처리
-//                    sendUnauthorizedResponse(response,"ACCESS_TOKEN_EXPIRED");
-//                    return;
-//                }
-//                case INVALID_SIGNATURE, MALFORMED, INVALID -> {
-//                    // 위변조, 형식 오류, 기타 오류는 모두 인증 실패로 처리 -> 재로그인 요청
-//                    // TODO: 저장된 Access Token, Refresh Token 쿠키 및 메모리에서 모두 삭제
-//                    sendUnauthorizedResponse(response,"INVALID_ACCESS_TOKEN");
-//                    return;
-//                }
-//                default -> {
-//                    // 재로그인 요청
-//                    log.info("status : Unknown token error");
-//                    sendUnauthorizedResponse(response,"UNKNOWN_TOKEN_ERROR");
-//                    return;
-//                }
-//            }
-//        }else{
-//            // RefreshToken 추출
-//            String refreshToken = extractRefreshTokenFromCookie(request);
-//
-//            if (refreshToken != null && !refreshToken.isBlank()) {
-//                sendUnauthorizedResponse(response,"ACCESS_TOKEN_EXPIRED");
-//                return;
-//            } else {
-//                log.info("No token provided in request.");
-//                sendUnauthorizedResponse(response,"TOKEN_MISSING");
-//                return;
-//            }
-//        }
+        if(accessToken != null){
+            TokenStatus status = jwtProvider.validateToken(accessToken);
+            log.info("TokenStatus : {}", status);
+
+            switch (status) {
+                case VALID -> {
+                    Claims claims = jwtProvider.parseToken(accessToken).getBody();
+                    Long id = Long.valueOf(claims.getSubject());
+                    String name = jwtProvider.validateAndGetClaim(claims,"name",String.class); //TODO: 값이 없을 경우의 예외처리 하기
+                    String role = jwtProvider.validateAndGetClaim(claims,"role",String.class);
+
+                    JwtUserDetails userDetails = new JwtUserDetails(id, name, role);
+
+                    Authentication authentication = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, List.of(new SimpleGrantedAuthority("ROLE_" + role)));
+
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                    filterChain.doFilter(request, response);
+                    return;
+                }
+                case EXPIRED -> {
+                    // 클라이언트에 토큰 만료 안내 -> 클라이언트 측에서 AccessToken 재발급 요청 처리
+                    sendUnauthorizedResponse(response,"ACCESS_TOKEN_EXPIRED");
+                    return;
+                }
+                case INVALID_SIGNATURE, MALFORMED, INVALID -> {
+                    // 위변조, 형식 오류, 기타 오류는 모두 인증 실패로 처리 -> 재로그인 요청
+                    // TODO: 저장된 Access Token, Refresh Token 쿠키 및 메모리에서 모두 삭제
+                    sendUnauthorizedResponse(response,"INVALID_ACCESS_TOKEN");
+                    return;
+                }
+                default -> {
+                    // 재로그인 요청
+                    log.info("status : Unknown token error");
+                    sendUnauthorizedResponse(response,"UNKNOWN_TOKEN_ERROR");
+                    return;
+                }
+            }
+        }else{
+            // RefreshToken 추출
+            String refreshToken = extractRefreshTokenFromCookie(request);
+
+            if (refreshToken != null && !refreshToken.isBlank()) {
+                sendUnauthorizedResponse(response,"ACCESS_TOKEN_EXPIRED");
+                return;
+            } else {
+                log.info("No token provided in request.");
+                sendUnauthorizedResponse(response,"TOKEN_MISSING");
+                return;
+            }
+        }
     }
 
     private void sendUnauthorizedResponse(HttpServletResponse response, String message) {
