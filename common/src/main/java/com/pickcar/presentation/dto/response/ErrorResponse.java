0a990c884380f8d2ef6638edb28c015fc5093c27
seomoon <sunghoon1216@naver.com>
2025-07-06 19:47:54 +0900

    refactor: off시 운행일지 작성 실패 예외를 파싱을 통해 근본적 원인을 노출할 수 있도록 하고, 4xx, 5xx 케이스를 상세히 다루도록 분할, traceId 전파 여부 체크 완료

--- common/src/main/java/com/pickcar/presentation/dto/response/ErrorResponse.java
+++ common/src/main/java/com/pickcar/presentation/dto/response/ErrorResponse.java
@@ -1,16 +1,43 @@
 package com.pickcar.presentation.dto.response;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.pickcar.exception.ErrorReason;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.client.HttpStatusCodeException;
 
+@Slf4j
 public record ErrorResponse(
         ResponseInfo responseInfo,
         ErrorReason errorReason
 ) {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
     public ErrorResponse(Integer statusCode, String errorCode, String reason) {
         this(ResponseInfo.error(statusCode), new ErrorReason(errorCode, reason));
     }
 
     public ErrorResponse(Integer statusCode, ErrorReason errorReason) {
         this(ResponseInfo.error(statusCode), errorReason);
+    }
+
+    public static Optional<ErrorResponse> parseHttpStatusCodeException(HttpStatusCodeException exception) {
+        String responseBody = exception.getResponseBodyAsString();
+
+        try {
+            ErrorResponse errorResponse = OBJECT_MAPPER.readValue(responseBody, ErrorResponse.class);
+            return Optional.of(errorResponse);
+        } catch (JsonProcessingException e) {
+            log.error("ERROR Response 파싱에 실패하였습니다. statusCode: {}, body: {}",
+                    exception.getStatusCode(), responseBody);
+
+            return Optional.empty();
+        }
     }
 }
--- consumer/src/main/java/com/pickcar/application/EventInfoService.java
+++ consumer/src/main/java/com/pickcar/application/EventInfoService.java
@@ -4,11 +4,15 @@ import com.pickcar.config.RestTemplateConfig;
 import com.pickcar.dto.EventPayload;
 import com.pickcar.emulator.domain.EventInfo;
 import com.pickcar.infrastructure.EventInfoRepository;
+import com.pickcar.presentation.dto.response.ErrorResponse;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
@@ -40,12 +44,15 @@ public class EventInfoService {
     private EventInfo saveEventInfo(EventPayload request) {
         log.info("Saving event info: {}", request);
         try {
-            Optional<EventInfo> getEventInfo = eventInfoRepository.findTopByVehicleIdOrderByIdDesc(request.getVehicleId());
+            Optional<EventInfo> getEventInfo = eventInfoRepository.findTopByVehicleIdOrderByIdDesc(
+                    request.getVehicleId());
 
             getEventInfo.ifPresent(info -> {
                 if (info.getEventStatus().equals(request.getEventStatus())) {
-                    log.error("EventInfo 저장 실패: {}", request);
-                    throw new RuntimeException("이미 동일한 상태입니다.");
+//                    log.error("EventInfo 저장 실패: {}", request);
+//                    throw new RuntimeException("이미 동일한 상태입니다.");
+                    log.warn("이미 동일한 상태입니다 : {}",request);
+                    return;     // 다시 예외를 던지지 않고 종료
                 }
             });
 
@@ -72,11 +79,26 @@ public class EventInfoService {
 
     public void writeDriveHistoryRequestAfterOff(EventInfo offEventInfo) {
         String url = deployDomain + "/api/v1/history/%d".formatted(offEventInfo.getId());
-        log.info("driving history request to: {}", url);
         try {
+            log.info("Request URL : {}, OffEventInfoId : {}", url, offEventInfo.getId());
             restTemplate.postForEntity(url, null, Void.class);
+            log.info("시동 OFF에 따른 운행일지 작성이 성공적으로 완료되었습니다. event ID : {}", offEventInfo.getId());
+        } catch (HttpClientErrorException e) {
+            Optional<ErrorResponse> errorResponse = ErrorResponse.parseHttpStatusCodeException(e);
+
+            if(errorResponse.isPresent()) {
+                log.warn("운행일지 작성에 실패하였습니다. reason : {}", errorResponse.get().errorReason().reason());
+                return;
+            }
+
+            log.warn("운행일지 작성과 요청 정보 파싱에 실패하였습니다. responseBody : {}", e.getResponseBodyAsString());
+        } catch (HttpServerErrorException e) {
+            log.error("서버 오류로 인해 운행일지 작성 요청에 실패하였습니다. event id : {}", offEventInfo.getId());
+        } catch (ResourceAccessException e) {
+            log.error("네트워크 오류로 인해 운행일지 작성 요청에 실패하였습니다. event id : {}, url : {}", offEventInfo.getId(), url);
+            //NOTE: 5XX 에러 (서버, 네트워크 등 오류시 재시도 여부 결정 필요)
         } catch (Exception e) {
-            log.error("EventInfo 전송 실패: {}", e.getMessage(), e);
+            // FIXME: 무한 요청 방지를 위한 임시 catch
         }
     }
 
